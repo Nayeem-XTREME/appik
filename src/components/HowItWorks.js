1a0370a2859f7d6c4fa186e8a818429b5ab19d85
Nayeem-Spring <niaz.nayeem@springrainit.com>
2021-01-22 15:46:35 +0600

    HowItWorks Responsive

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -4,6 +4,7 @@ import { Box, Flex, Text } from "../components"
 import { Wrapper, Title } from '../styles/MyStyles'
 import ListItem from './ui/ListItem'
 import { how } from '../data/how'
+import theme from '../styles/theme'
 
 import control from '../assets/logo/Control.svg'
 import bg from '../assets/img/bg.svg'
@@ -17,9 +18,21 @@ const Bg = styled.div`
 `
 
 const Image = styled.img`
+    width: 100%;
     max-width: ${props => props.maxWidth};
     height: ${props => props.height};
     margin-left: ${props => props.marginLeft};
+
+    @media only screen and (max-width: ${theme.breakpoints.lg}) {
+        margin: 0 auto;
+        margin-bottom: 75px;
+        display: flex;
+        justify-content: center;
+    }
+
+    @media only screen and (max-width: ${theme.breakpoints.md}) {
+        display: none;
+    }
 `
 
 export default function HowItWorks() {
@@ -32,11 +45,11 @@ export default function HowItWorks() {
                 </Title>
 
                 <Flex flexWrap="wrap">
-                    <Box width={1/2}>
-                        <Image maxWidth="42%" height="auto" marginLeft="130px" src={control} alt="Control" />
+                    <Box width={[1, 1, 1, 1/2]}>
+                        <Image maxWidth="240px" height="auto" marginLeft="130px" src={control} alt="Control" />
                     </Box>
 
-                    <Box width={1/2}>
+                    <Box width={[1, 1, 1, 1/2]}>
                         { how.map( (item, index) => {
                             if (index === how.length - 1)
                                 return <ListItem key={index} img={item.src} alt={item.alt} title={item.title} details={item.details} />
