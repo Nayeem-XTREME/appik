7e0a3e136829bd50614cbdf7e655b863b23d44e4
Niaz Morshed Nayeem <nayeemxtreme@live.com>
2021-01-26 19:09:04 +0600

    Doing Pricing section for Mobile

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -1,64 +1,94 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react"
+import styled from "styled-components"
 import { Box, Flex, Text } from "../components"
-import { Wrapper, Title } from '../styles/MyStyles'
-import ListItem from './ui/ListItem'
-import { how } from '../data/how'
-import theme from '../styles/theme'
+import { Wrapper, Title } from "../styles/MyStyles"
+import ListItem from "./ui/ListItem"
+import { how } from "../data/how"
+import theme from "../styles/theme"
 
-import control from '../assets/logo/Control.svg'
-import bg from '../assets/img/bg.svg'
-
+import control from "../assets/logo/Control.svg"
+import bg from "../assets/img/bg.svg"
 
 const Background = styled.div`
-    background-image: url(${bg});
-    background-repeat: no-repeat;
-    background-position: 0 220px;
-    background-size: auto 460px;
+  background-image: url(${bg});
+  background-repeat: no-repeat;
+  background-position: 0 220px;
+  background-size: auto 460px;
+
+  @media only screen and (max-width: ${theme.breakpoints.md}) {
+      background: none;
+  }
 `
 
 const Image = styled.img`
-    width: 100%;
-    max-width: ${props => props.maxWidth};
-    height: ${props => props.height};
-    margin-left: ${props => props.marginLeft};
+  width: 100%;
+  max-width: ${props => props.maxWidth};
+  height: ${props => props.height};
+  margin-left: ${props => props.marginLeft};
 
-    @media only screen and (max-width: ${theme.breakpoints.lg}) {
-        margin: 0 auto;
-        margin-bottom: 75px;
-        display: flex;
-        justify-content: center;
-    }
+  @media only screen and (max-width: ${theme.breakpoints.lg}) {
+    margin: 0 auto;
+    margin-bottom: 75px;
+    display: flex;
+    justify-content: center;
+  }
 
-    @media only screen and (max-width: ${theme.breakpoints.md}) {
-        display: none;
-    }
+  @media only screen and (max-width: ${theme.breakpoints.md}) {
+    display: none;
+  }
 `
 
 export default function HowItWorks() {
-    return (
-        <Background>
-            <Wrapper>
-                <Title>
-                    <Text variant="h2">How it Works</Text>
-                    <Text variant="p">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words</Text>
-                </Title>
+  return (
+    <Background>
+      <Wrapper>
+        <Title>
+          <Text variant="h2">How it Works</Text>
+          <Text variant="p">
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words
+          </Text>
+        </Title>
 
-                <Flex flexWrap="wrap">
-                    <Box width={[1, 1, 1, 1/2]}>
-                        <Image maxWidth="240px" height="auto" marginLeft="130px" src={control} alt="Control" />
-                    </Box>
+        <Flex flexWrap="wrap">
+          <Box width={[1, 1, 1, 1 / 2]}>
+            <Image
+              maxWidth="240px"
+              height="auto"
+              marginLeft="130px"
+              src={control}
+              alt="Control"
+            />
+          </Box>
 
-                    <Box width={[1, 1, 1, 1/2]}>
-                        { how.map( (item, index) => {
-                            if (index === how.length - 1)
-                                return <ListItem key={index} img={item.src} alt={item.alt} title={item.title} details={item.details} />
-                            else 
-                                return <ListItem mb key={index} img={item.src} alt={item.alt} title={item.title} details={item.details} />
-                        } ) }
-                    </Box>
-                </Flex>
-            </Wrapper>
-        </Background>
-    )
+          <Box width={[1, 1, 1, 1 / 2]}>
+            {how.map((item, index) => {
+              if (index === how.length - 1)
+                return (
+                  <ListItem
+                    key={index}
+                    img={item.src}
+                    alt={item.alt}
+                    title={item.title}
+                    details={item.details}
+                  />
+                )
+              else
+                return (
+                  <ListItem
+                    mb
+                    key={index}
+                    img={item.src}
+                    alt={item.alt}
+                    title={item.title}
+                    details={item.details}
+                  />
+                )
+            })}
+          </Box>
+        </Flex>
+      </Wrapper>
+    </Background>
+  )
 }
--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,259 +1,358 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
+import React, { Component } from "react"
+import styled from "styled-components"
 
 import { Box, Flex, Text } from "../components"
-import { Wrapper, Title, Button } from '../styles/MyStyles'
-import theme from '../styles/theme'
-import { prices } from '../data/prices'
+import { Wrapper, Title, Button } from "../styles/MyStyles"
+import theme from "../styles/theme"
+import { prices } from "../data/prices"
 
 const Div = styled.div`
-    position: relative;
+  position: relative;
 
-    .monthly-active {
-        position: absolute;
-        width: 100%;
-        left: 0;
-        transition: all 0.5s;
-    }
+  .monthly-active {
+    position: absolute;
+    width: 100%;
+    left: 0;
+    transition: all 0.5s;
+  }
 
-    .monthly-hidden {
-        position: absolute;
-        width: 100%;
-        left: -1500px;
-        transition: all 0.5s;
-    }
+  .monthly-hidden {
+    position: absolute;
+    width: 100%;
+    left: -1500px;
+    transition: all 0.5s;
+  }
 
-    .yearly-active {
-        position: absolute;
-        width: 100%;
-        left: 0;
-        transition: all 0.5s;
-    }
+  .yearly-active {
+    position: absolute;
+    width: 100%;
+    left: 0;
+    transition: all 0.5s;
+  }
 
-    .yearly-hidden {
-        position: absolute;
-        width: 100%;
-        left: 1500px;
-        transition: all 0.5s;
+  .yearly-hidden {
+    position: absolute;
+    width: 100%;
+    left: 1500px;
+    transition: all 0.5s;
+  }
+
+  &.onlyMobile {
+    display: none;
+    @media only screen and (max-width: ${theme.breakpoints.md}) {
+      display: block;
     }
+  }
 `
 
 const Card = styled.div`
-    border: 1px solid ${theme.colors.lightGray};
-    background-color: ${theme.colors.white};
-    border-radius: 10px;
-    padding: auto 10px;
-    transition: all 0.2s;
+  border: 1px solid ${theme.colors.lightGray};
+  background-color: ${theme.colors.white};
+  border-radius: 10px;
+  padding: auto 10px;
+  transition: all 0.2s;
 
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 
-    .highlight:after {
-        background-color: ${theme.colors.fill};
-    }
+  .highlight:after {
+    background-color: ${theme.colors.fill};
+  }
 
-    :hover .underline:after {
-        background-color: ${theme.colors.fill};
-    }
+  :hover .underline:after {
+    background-color: ${theme.colors.fill};
+  }
 `
 
 const MainTitle = styled.div`
-    text-align: center;
-    padding-top: 33px;
+  text-align: center;
+  padding-top: 33px;
 
-    :after {
-        content: "";
-        display: block;
-        width: 80%;
-        height: 3px;
-        background-color: ${theme.colors.lightGray};
-        border-radius: 10px;
-        margin: 33px auto 0 auto;
-        transition: all 0.2s;
-    }
+  :after {
+    content: "";
+    display: block;
+    width: 80%;
+    height: 3px;
+    background-color: ${theme.colors.lightGray};
+    border-radius: 10px;
+    margin: 33px auto 0 auto;
+    transition: all 0.2s;
+  }
 `
 
 const SubTitle = styled.div`
-    text-align: center;
-    margin-top: 33px;
+  text-align: center;
+  margin-top: 33px;
 
-    :after {
-        content: "";
-        display: block;
-        width: 50%;
-        height: 0.75px;
-        background-color: ${theme.colors.veryLightGray};
-        border-radius: 10px;
-        margin: 33px auto 0 auto;
-    }
+  :after {
+    content: "";
+    display: block;
+    width: 50%;
+    height: 0.75px;
+    background-color: ${theme.colors.veryLightGray};
+    border-radius: 10px;
+    margin: 33px auto 0 auto;
+  }
 `
 
 const Ul = styled.ul`
-    list-style: none;
-    text-align: center;
-    padding: 0;
-    margin: 24px 0 56px 0;
+  list-style: none;
+  text-align: center;
+  padding: 0;
+  margin: 24px 0 56px 0;
 `
 
 const Bg = styled.div`
-    background-color: ${theme.colors.lightGray};
-    width: 100%;
-    margin-top: -380px;
-    margin-bottom: 75px;
-    height: 340px;
-    z-index: -100;
+  background-color: ${theme.colors.lightGray};
+  width: 100%;
+  margin-top: -380px;
+  margin-bottom: 75px;
+  height: 340px;
+  z-index: -100;
 `
 
 const MyText = styled(Text)`
-    font-family: 'Roboto', sans-serif;
-    font-weight: 400;
-    color: ${theme.colors.defaultText};
-    line-height: 30px;
+  font-family: "Roboto", sans-serif;
+  font-weight: 400;
+  color: ${theme.colors.defaultText};
+  line-height: 30px;
 `
 
 const MyButton = styled(Button)`
-    display: inline-block;
-    margin: 0 auto 62px;
+  display: inline-block;
+  margin: 0 auto 62px;
 `
 
 const ButtonBox = styled.div`
-    width: 360px;
-    margin: -24px auto 46px;
-    position: relative;
-    border: 0.5px solid ${theme.colors.gray3};
-    border-radius: 30px;
-    transition: all 0.2s;
+  max-width: 360px;
+  margin: -24px auto 46px;
+  position: relative;
+  border: ${({ border }) => (border ? "0.5px solid #D1D1D1" : "none")};
+  border-radius: 30px;
+  transition: all 0.2s;
 
-    .btn-monthly {
-        top: 0;
-        left: 0;
-        transition: all 0.5s;
-    }
+  display: ${({ onlyMobile }) => (onlyMobile ? "none" : "block")};
 
-    .btn-yearly {
-        top: 0;
-        right: 0;
-        transition: all 0.5s;
-    }
+  .btn-monthly {
+    top: 0;
+    left: 0;
+    transition: all 0.5s;
+  }
+
+  .btn-yearly {
+    top: 0;
+    right: 0;
+    transition: all 0.5s;
+  }
+
+  @media only screen and (max-width: 768px) {
+    display: block;
+  }
 `
 
 const BtnBackground = styled.div`
-    position: absolute;
-    height: 100%;
-    width: 50%;
-    border-radius: 30px;
-    background-color: ${theme.colors.highlight};
-    transition: all 0.5s;
+  position: absolute;
+  height: 100%;
+  width: ${({ width }) => width};
+  border-radius: 30px;
+  background-color: ${theme.colors.highlight};
+  transition: all 0.5s;
 `
 
 const ToggleButton = styled.button`
-    padding: 12px 0;
-    width: 50%;
-    cursor: pointer;
-    background: transparent;
-    border: none;
-    outline: none;
-    position: relative;
+  padding: 12px 0;
+  width: ${({ width }) => width};
+  cursor: pointer;
+  background: transparent;
+  border: none;
+  outline: none;
+  position: relative;
 
-    text-align: center;
-    transition: all 0.2s;
+  text-align: center;
+  transition: all 0.2s;
+`
+
+const PriceBox = styled(Box)`
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+    display: ${({ active }) => (active ? "block" : "none")};
+  }
 `
 
 export default class Pricing extends Component {
+  state = {
+    monthly: "monthly-active",
+    yearly: "yearly-hidden",
+    activeButton: "btn-monthly",
 
-    state = {
-        monthly: "monthly-active",
-        yearly: "yearly-hidden",
-        activeButton: "btn-monthly"
-    }
+    type: "advanced",
+  }
 
-    monthlyHandler = () => {
-        this.setState({ monthly: "monthly-active", yearly: "yearly-hidden", activeButton: "btn-monthly" });
-    }
+  monthlyHandler = () => {
+    this.setState({
+      monthly: "monthly-active",
+      yearly: "yearly-hidden",
+      activeButton: "btn-monthly",
+    })
+  }
 
-    yearlyHandler = () => {
-        this.setState({ monthly: "monthly-hidden", yearly: "yearly-active", activeButton: "btn-yearly" });
-    }
+  yearlyHandler = () => {
+    this.setState({
+      monthly: "monthly-hidden",
+      yearly: "yearly-active",
+      activeButton: "btn-yearly",
+    })
+  }
 
-    render() {
-        return (
-            <>
-                <Wrapper style={{ height: "950px", overflow: "hidden" }}>
-                    <Title>
-                        <Text variant="h2">Appik Prices</Text>
-                        <Text variant="p">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised.</Text>
-                    </Title>
-    
-                    <ButtonBox>
-                        <BtnBackground className={this.state.activeButton}/>
-                        <ToggleButton onClick={this.monthlyHandler}>
-                            <Text fontSize={16} fontFamily='"Montserrat", sans-serif' fontWeight={700} color="black">MONTHLY</Text>
-                        </ToggleButton> 
-                        <ToggleButton onClick={this.yearlyHandler}>
-                            <Text fontSize={16} fontFamily='"Montserrat", sans-serif' fontWeight={700} color="black">YEARLY</Text>
-                        </ToggleButton>
-                    </ButtonBox>
-    
-                    <Div>
-                        <Flex flexWrap="wrap" className={this.state.monthly}>
-                            { prices.map( (item, index) => {
-                                return (
-                                    <Box width={[1, 1, 1/3]} key={index} px={10}>
-                                        <Card>
-                                            <MainTitle className={item.highlight ? "underline highlight" : "underline"}>
-                                                <Text variant="h3">{item.title}</Text>
-                                            </MainTitle>
-    
-                                            <SubTitle>
-                                                <Text variant="h4">{item.monthlyPrice}</Text>
-                                            </SubTitle>
-    
-                                            <Ul>
-                                                { item.features.map( (feature, ind) => (
-                                                    <li key={ind}>
-                                                        <MyText variant="p">{feature}</MyText>
-                                                    </li>
-                                                ) ) }
-                                            </Ul>
-    
-                                            <MyButton highlight={item.highlight}>BUY NOW</MyButton>
-                                        </Card>
-                                    </Box>
-                                )
-                            } ) }
-                        </Flex>
-    
-                        <Flex flexWrap="wrap" className={this.state.yearly}>
-                            { prices.map( (item, index) => {
-                                return (
-                                    <Box width={[1, 1, 1/3]} key={index} px={10}>
-                                        <Card>
-                                            <MainTitle className={item.highlight ? "underline highlight" : "underline"}>
-                                                <Text variant="h3">{item.title}</Text>
-                                            </MainTitle>
-    
-                                            <SubTitle>
-                                                <Text variant="h4">{item.yearlyPrice}</Text>
-                                            </SubTitle>
-    
-                                            <Ul>
-                                                { item.features.map( (feature, ind) => (
-                                                    <li key={ind}>
-                                                        <MyText variant="p">{feature}</MyText>
-                                                    </li>
-                                                ) ) }
-                                            </Ul>
-    
-                                            <MyButton highlight={item.highlight}>BUY NOW</MyButton>
-                                        </Card>
-                                    </Box>
-                                )
-                            } ) }
-                        </Flex>
-                    </Div>
-                </Wrapper>
-                <Bg />
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <Wrapper style={{ height: "950px", overflow: "hidden" }}>
+          <Title>
+            <Text variant="h2">Appik Prices</Text>
+            <Text variant="p">
+              There are many variations of passages of Lorem Ipsum available,
+              but the majority have suffered alteration in some form, by
+              injected humour, or randomised.
+            </Text>
+          </Title>
+
+          <ButtonBox border>
+            <BtnBackground width="50%" className={this.state.activeButton} />
+            <ToggleButton width="50%" onClick={this.monthlyHandler}>
+              <Text
+                fontSize={16}
+                fontFamily='"Montserrat", sans-serif'
+                fontWeight={700}
+                color="black"
+              >
+                MONTHLY
+              </Text>
+            </ToggleButton>
+            <ToggleButton width="50%" onClick={this.yearlyHandler}>
+              <Text
+                fontSize={16}
+                fontFamily='"Montserrat", sans-serif'
+                fontWeight={700}
+                color="black"
+              >
+                YEARLY
+              </Text>
+            </ToggleButton>
+          </ButtonBox>
+
+          <ButtonBox onlyMobile>
+            <BtnBackground width="33.3333333%" />
+            <ToggleButton width="33.3333333%">
+              <Text
+                fontSize={16}
+                fontFamily='"Montserrat", sans-serif'
+                fontWeight={700}
+                color="black"
+              >
+                BASIC
+              </Text>
+            </ToggleButton>
+            <ToggleButton width="33.3333333%">
+              <Text
+                fontSize={16}
+                fontFamily='"Montserrat", sans-serif'
+                fontWeight={700}
+                color="black"
+              >
+                ADVANCED
+              </Text>
+            </ToggleButton>
+            <ToggleButton width="33.3333333%">
+              <Text
+                fontSize={16}
+                fontFamily='"Montserrat", sans-serif'
+                fontWeight={700}
+                color="black"
+              >
+                PREMIUM
+              </Text>
+            </ToggleButton>
+          </ButtonBox>
+
+          <Div>
+            <Flex flexWrap="wrap" className={this.state.monthly}>
+              {prices.map((item, index) => {
+                return (
+                  <PriceBox
+                    active={item.highlight ? true : false}
+                    width={[1, 1, 1 / 3]}
+                    key={index}
+                    px={10}
+                  >
+                    <Card>
+                      <MainTitle
+                        className={
+                          item.highlight ? "underline highlight" : "underline"
+                        }
+                      >
+                        <Text variant="h3">{item.title}</Text>
+                      </MainTitle>
+
+                      <SubTitle>
+                        <Text variant="h4">{item.monthlyPrice}</Text>
+                      </SubTitle>
+
+                      <Ul>
+                        {item.features.map((feature, ind) => (
+                          <li key={ind}>
+                            <MyText variant="p">{feature}</MyText>
+                          </li>
+                        ))}
+                      </Ul>
+
+                      <MyButton highlight={item.highlight}>BUY NOW</MyButton>
+                    </Card>
+                  </PriceBox>
+                )
+              })}
+            </Flex>
+
+            <Flex flexWrap="wrap" className={this.state.yearly}>
+              {prices.map((item, index) => {
+                return (
+                  <PriceBox
+                    active={item.highlight ? true : false}
+                    width={[1, 1, 1 / 3]}
+                    key={index}
+                    px={10}
+                  >
+                    <Card>
+                      <MainTitle
+                        className={
+                          item.highlight ? "underline highlight" : "underline"
+                        }
+                      >
+                        <Text variant="h3">{item.title}</Text>
+                      </MainTitle>
+
+                      <SubTitle>
+                        <Text variant="h4">{item.yearlyPrice}</Text>
+                      </SubTitle>
+
+                      <Ul>
+                        {item.features.map((feature, ind) => (
+                          <li key={ind}>
+                            <MyText variant="p">{feature}</MyText>
+                          </li>
+                        ))}
+                      </Ul>
+
+                      <MyButton highlight={item.highlight}>BUY NOW</MyButton>
+                    </Card>
+                  </PriceBox>
+                )
+              })}
+            </Flex>
+          </Div>
+        </Wrapper>
+        <Bg />
+      </>
+    )
+  }
 }
