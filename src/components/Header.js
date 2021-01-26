fa2a0306578a932f9c682961590df314fc6eddab
Nayeem-Spring <niaz.nayeem@springrainit.com>
2021-01-26 14:41:11 +0600

    Navbar RESPONSIVE

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
-import { FaBars } from 'react-icons/fa'
+import { FaBars, FaMinus } from 'react-icons/fa'
+
+import MobileNav from './MobileNav'
 import theme from '../styles/theme'
 import { Wrapper } from '../styles/MyStyles'
 
@@ -12,8 +14,14 @@ import { navmenu } from '../data/navmenu'
 const Background = styled.div`
   position: fixed;
   width: 100%;
-  background-color: black;
+  background: transparent;
   z-index: 900;
+  transition: all 0.2s;
+
+  &.active {
+    background: linear-gradient(rgba(35, 79, 167, .9), rgba(110, 79, 204, .9));
+    transition: all 0.2s ease-in-out;
+  }
 `
 
 const MyWrapper = styled(Wrapper)`
@@ -37,24 +45,19 @@ const NavLink = styled(Link)`
   font-size: ${theme.fontSizes[2]}px;
   transition: all 0.2s;
 
-  &.active {
-    color: ${theme.colors.highlight};
-  }
-
   :not(:first-child) {
     margin-left: 30px;
   }
 
   :hover {
     color: ${theme.colors.highlight};
-    text-decoration: none;
   }
 `
 
 const NavMenu = styled.div`
   display: flex;
 
-  @media only screen and (max-width: ${theme.breakpoints.lg}) {
+  @media only screen and (max-width: 1100px) {
     display: none;
   }
 `
@@ -62,8 +65,21 @@ const NavMenu = styled.div`
 const Bars = styled(FaBars)`
   color: #FFF;
   display: none;
+  transition: all 0.2s;
 
-  @media only screen and (max-width: ${theme.breakpoints.lg}) {
+  @media only screen and (max-width: 1100px) {
+    display: block;
+    font-size: 30px;
+    cursor: pointer;
+  }
+`
+
+const Close = styled(FaMinus)`
+  color: #FFF;
+  display: none;
+  transition: all 0.2s;
+
+  @media only screen and (max-width: 1100px) {
     display: block;
     font-size: 30px;
     cursor: pointer;
@@ -77,25 +93,45 @@ const Image = styled.img`
 class Header extends Component {
 
   state = {
-    
+    clicked: false,
+    navbarActive: false
+  }
+
+  mobileMenuHandler = () => {
+    this.setState({ clicked: !this.state.clicked });
+    console.log(this.state.clicked);
+  }
+
+  changeNavBackground = () => {
+    if (window.scrollY > 60) {
+      this.setState({ navbarActive: true });
+    } else {
+      this.setState({ navbarActive: false });
+    }
   }
 
   render() {
+
+    window.addEventListener('scroll', this.changeNavBackground);
+
     return (
-      <Background>
-        <MyWrapper>
-          <Nav>
-            <NavLink to="/"> <Image src={logo} alt="APPIK"/> </NavLink>
-            <Bars />
-            <NavMenu className="active">
-              {navmenu.map( (x, i) => <NavLink className="navlink" to={x.link} key={i} activeStyle> {x.title} </NavLink> )}
-            </NavMenu>
-          </Nav>
-        </MyWrapper>
-      </Background>
+      <>
+        <Background className={this.state.navbarActive ? 'active' : ''}>
+          <MyWrapper>
+            <Nav>
+              <NavLink to="/"> <Image src={logo} alt="APPIK"/> </NavLink>
+              { this.state.clicked ? <Close onClick={this.mobileMenuHandler} /> : <Bars onClick={this.mobileMenuHandler} />}
+              <NavMenu className="active">
+                {navmenu.map( (x, i) => <NavLink className="navlink" to={x.link} key={i} > {x.title} </NavLink> )}
+              </NavMenu>
+            </Nav>
+          </MyWrapper>
+        </Background>
+
+        <MobileNav status={ this.state.clicked === true ? "active" : "closed" } />
+      </>
     )
   }
-
 }
 
 export default Header
---
+++ src/components/MobileNav.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import { Link } from "gatsby"
+import styled from "styled-components"
+import { Wrapper } from '../styles/MyStyles'
+import theme from '../styles/theme'
+
+import { navmenu } from '../data/navmenu'
+import { platform } from '../data/platform'
+
+const Nav = styled.nav`
+  background: linear-gradient(rgba(110, 79, 204, 1), rgba(35, 79, 167, 1));
+  width: 100%;
+  position: fixed;
+  z-index: 200;
+  transition: all 0.3s;
+
+  &.active {
+    transform: translateY(0%);
+    transition: all 0.3s;
+  }
+
+  &.closed {
+    transform: translateY(-100%);
+    transition: all 0.3s;
+  }
+`
+
+const MyWrapper = styled(Wrapper)`
+  position: relative;
+  padding: 0;
+  height: 100vh; 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const NavMenu = styled.div`
+  padding-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const NavLink = styled(Link)`
+  margin: 15px 0;
+  list-style: none;
+  text-decoration: none;
+  color: #FFF;
+  transition: all 0.2s;
+
+  :hover {
+    color: ${theme.colors.highlight};
+  }
+`
+
+const Support = styled.div`
+  position: absolute;
+  bottom: 80px;
+  left: 60px;
+`
+
+const Image = styled.img`
+  max-width: 46px;
+  height: auto;
+  margin-right: 40px;
+`
+
+export default function MobileNav({ status }) {
+  return (
+    <Nav className={status}>
+      <MyWrapper>
+        <NavMenu className="active">
+          {navmenu.map( (x, i) => <NavLink className="navlink" to={x.link} key={i} > {x.title} </NavLink> )}
+        </NavMenu>
+        <Support>
+          {platform.map((logo, i) => (
+            <Image key={i} src={logo.src} alt={logo.alt} />
+          ))}
+        </Support>
+      </MyWrapper>
+    </Nav>
+  )
+}
